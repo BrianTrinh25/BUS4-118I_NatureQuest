6577d3efab869a94cd33f00b15aba1dc5ac9bdad
Denise Christy <deniz.christy@sjsu.edu>
2024-10-24 13:02:51 -0700

    hiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ map_prototype.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Hiking Map Prototype")
+st.sidebar.markdown("# Hiking Map Prototype")
+
+zip = st.number_input(
+    "Zip code", value=None, placeholder="Zip Code"
+)
+
+distance = st.selectbox(
+    "Distance",
+    ("5 mi", "10 mi", "25 mi", "50 mi"),
+)
+
+# Update your DataFrame with the specific coordinates
+df = pd.DataFrame({
+    'lat': [37.307226530422035],
+    'lon': [-121.9137624590682]
+})
+
+# Plot the map with the updated DataFrame
+st.map(df)
+
